2da8bf6ea84e76cf0f8e60efa8dfdde887158b8d
Your Name <thev@itu.dk>
2026-09-15 13:17:48 +0200

    task1-handshake

--- 3wayhandshake/main.go
+++ 3wayhandshake/main.go
@@ -1,32 +1,50 @@
-package wayhandshake
+package main
+
 //3 way tcp handshake
-import ("fmt",
-"sync"
-);
+import (
+	"fmt"
+	"sync"
+)
 
+var wg sync.WaitGroup
 
-
-
-func server(syn <- chan string, ack <- chan string, synack <- chan string){
+func server(syn <-chan string, ack <-chan string, synack chan<- string) {
 	// this tells the waitgroup that the server goroutine is done when it returns
 	// the defer makes it like always run even if there is a error.
 	defer wg.Done()
-	msg := <- syn
 
+	msg := <-syn
 	fmt.Println("Server has got the message: ", msg)
-	
+
+	synack <- "SYN-ACK"
+	fmt.Println("Server has sent the message: SYN-ACK")
+
+	msg2 := <-ack
+	fmt.Println("Server has got the message: ", msg2)
 }
 
-func client(syn <- chan string, ack <- chan string, synack <- chan string){
+func client(syn chan<- string, ack chan<- string, synack <-chan string) {
 	defer wg.Done()
-	syn <- "SYN"
+
+	syn <- "SYN" //Client -> server
 	fmt.Println("Client has sent the message: SYN")
+
+	<-synack //Client waits for SYN-ACK
+	fmt.Println("Client has got the message: SYN-ACK")
+
+	ack <- "ACK" //Client -> server
+	fmt.Println("Client has sent the message: ACK")
 }
-
-
 
 func main() {
+	syn := make(chan string)
+	ack := make(chan string)
+	synack := make(chan string)
 
-	
+	wg.Add(2)
+
+	go client(syn, ack, synack)
+	go server(syn, ack, synack)
+
+	wg.Wait()
 }
-	
